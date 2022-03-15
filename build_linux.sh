4ce86c9b5077a944cab9165c2461bd1d1283f5fb
Candido Tominaga <candido1212@gmail.com>
2022-03-15 15:53:07 -0300

    build in linux mint 20.3

--- build_linux.sh
+++ build_linux.sh
@@ -8,11 +8,15 @@ set -eux
 : "${OBS_SRC_DIR:=/usr/include/obs}"
 
 ver=$(grep 'obs-multi-rtmp VERSION' CMakeLists.txt)
-ver=($ver)
-ver=${ver[2]}
-ver=${ver:0:-1}
+ver=${ver:30:-1}
 
 rm -fr build dist *.tar.xz
+
+# have cmake?
+apt install cmake
+
+# have qt5 lib?
+sudo apt-get install qtcreator qt5-default
 
 cmake  -DCMAKE_INSTALL_PREFIX="$DESTDIR" -DQTDIR="$QTDIR" -DOBS_SRC_DIR="$OBS_SRC_DIR" -B build .
 cmake --build build --config Release
